f65b0ab60088b8c94f0f7a806d415237e5b855a6
Rhythm <65298117+Rhythm1710@users.noreply.github.com>
2024-06-18 17:12:42 +0530

    Respond to multiple Pings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-// Uncomment this block to pass the first stage
 use std::{
-    io::{Read,Write},
-    net::TcpListener,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
 };
 
+use anyhow::Result;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
@@ -14,10 +13,9 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
-
-                stream.write(b"+PONG\r\n").unwrap();
+                if let Err(e) = handle_connection(&mut stream) {
+                    println!("error: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -26,35 +24,30 @@ fn main() {
     }
 }
 
+fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    loop {
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    continue;
+                }
+            }
+            Err(e) => {
+                println!("err: {}", e);
+                continue;
+            }
+        }
+        if let Err(e) = handle_request(stream) {
+            println!("err: {}", e);
+            continue;
+        }
+    }
+}
 
-// use std::io::{Read, Write};
-// use std::net::{TcpListener, TcpStream};
-
-// fn main() {
-//     // Creates a TCP server listening on localhost:8080
-//     let listener = TcpListener::bind("localhost:8080").expect("Could not bind");
-
-//     for stream in listener.incoming() {
-//         match stream {
-//             Ok(stream) => {
-//                 handle_client(stream);
-//             }
-//             Err(e) => {
-//                 eprintln!("Failed: {}", e);
-//             }
-//         }
-//     }
-// }
-
-// fn handle_client(mut stream: TcpStream) {
-//     let mut buf = [0; 512];
-//     loop {
-//         let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
-
-//         if bytes_read == 0 {
-//             return;
-//         }
-
-//         stream.write_all(&buf[0..bytes_read]).expect("Failed to write to client");
-//     }
-// }
+fn handle_request(stream: &mut TcpStream) -> std::io::Result<()> {
+    let response = b"+PONG\r\n";
+    stream.write_all(response)?;
+    stream.flush()?;
+    Ok(())
+}
