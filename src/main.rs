388f018c50578f9567788edd3cdb8fdb1fed57d2
Rhythm <65298117+Rhythm1710@users.noreply.github.com>
2024-06-18 18:05:52 +0530

    Multiple concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,43 @@
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 use anyhow::Result;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                if let Err(e) = handle_connection(&mut stream) {
-                    println!("error: {}", e);
-                }
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        println!("Accepted new connection");
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                println!("Error: {}", e);
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
 
-fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
     loop {
         let mut buffer = [0; 1024];
-        match stream.read(&mut buffer) {
-            Ok(bytes_read) => {
-                if bytes_read == 0 {
-                    continue;
-                }
-            }
-            Err(e) => {
-                println!("err: {}", e);
-                continue;
-            }
+        let bytes_read = stream.read(&mut buffer).await?;
+        if bytes_read == 0 {
+            break;
         }
-        if let Err(e) = handle_request(stream) {
-            println!("err: {}", e);
-            continue;
+
+        if let Err(e) = handle_request(&mut stream).await {
+            println!("Error: {}", e);
         }
     }
+    Ok(())
 }
 
-fn handle_request(stream: &mut TcpStream) -> std::io::Result<()> {
+async fn handle_request(stream: &mut TcpStream) -> Result<()> {
     let response = b"+PONG\r\n";
-    stream.write_all(response)?;
-    stream.flush()?;
+    stream.write_all(response).await?;
+    stream.flush().await?;
     Ok(())
 }
 //Testing
